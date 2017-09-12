26dd817d096c92911ef0750a0dbfa38e39ec288e
Kajal Doshi <kajjaldoshi@gmail.com>
2017-09-12 19:04:50 +0530

    Week-3 Assignment

---
+++ Interfacing with the Arduino/Week 3/eeprom_serial.ino
@@ -0,0 +1,27 @@
+#include <EEPROM.h>
+
+String val;
+int value;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+if(Serial.available()){
+  val = Serial.readString();
+
+  if(val.startsWith("read")){
+    String address = val.substring(5);
+    value = EEPROM.read(address.toInt());
+    Serial.print(value);
+  }
+  else if(val.startsWith("write")){
+    val = val.substring(6);
+    int index = val.indexOf(" ");
+    String address = val.substring(0,index);
+    String ent = val.substring(index+1);
+    EEPROM.write(address.toInt(), ent.toInt());
+  }
+} 
+}
